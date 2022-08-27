9d96166a39959dc4e30cd9306539ba84eb4e0e03
gekas145 <56126336+gekas145@users.noreply.github.com>
2022-08-27 22:45:03 +0200

    update draw app

--- python_version/draw_app.py
+++ python_version/draw_app.py
@@ -1,39 +1,40 @@
 import network2
 import numpy as np
-# net = network2.Network([784, 100, 10])
-# net.gradient_descent()
-net = network2.Network.load('network2.json')
-from PIL import ImageTk, Image, ImageDraw
+from PIL import Image, ImageDraw
 import PIL
 from tkinter import *
 import matplotlib.pyplot as plt
+
+net = network2.Network.load('network2.json')
+
 
 # App for creating and examining own examples for MNIST handwritten digits recognition problem
 
 def trunc(values, decs=0):
     """ Leaves `decs` digits after . in real numbers(`values`) """
-    return np.trunc(values*10**decs)/(10**decs)
+    return np.trunc(values * 10 ** decs) / (10 ** decs)
 
 
+# tkinter app parameters
 width, height = 200, 200
-center = height//2
-white = (255, 255, 255)
-green = (0,128,0)
-
-
-# tkinter app parameters above
+center = height // 2
+white = 'white'
+black = 'black'
+background_color = white
+drawing_color = black
+zoom = 1
 
 
 def submit():
     """ Converts drawn image to 784 dim numpy array and prints out network's decision"""
-    data = image1.convert('L')
+    data = drawing.convert('L')
     data = data.resize((28, 28), PIL.Image.ANTIALIAS)
     data = np.array(data)
     data = np.true_divide(data, 255)
     data *= -1
     data += 1
-    data = trunc(data, 1)
-    res = net.feedforward(data.reshape(28**2))
+    data = trunc(data, 2)
+    res = net.feedforward(data.reshape(28 ** 2))
     for i in range(len(res)):
         if res[i] >= 0.1:
             print(f"{i}:        {np.round(res[i], 3)} \n")
@@ -41,27 +42,32 @@ def submit():
     plt.imshow(data, cmap=plt.cm.binary)
     plt.show()
 
+
 def paint(event):
     """ Draws image """
     x1, y1 = (event.x - 8), (event.y - 8)
     x2, y2 = (event.x + 8), (event.y + 8)
-    cv.create_oval(x1, y1, x2, y2, fill="black",width=5)
-    draw.line([x1, y1, x2, y2],fill="black",width=5)
+    cv.create_rectangle(x1, y1, x2, y2, fill=drawing_color, width=5)
+    draw.line([x1/zoom, y1/zoom, x2/zoom, y2/zoom], fill=drawing_color, width=5)
 
 
+def erase(event):
+    """ Erase canvas contents """
+    cv.delete("all")
+    draw.rectangle([0, 0, width, height], fill=background_color)
 
-while True:
-    root = Tk()
 
-    cv = Canvas(root, width=width, height=height, bg='white')
-    cv.pack()
+root = Tk()
 
-    image1 = PIL.Image.new("RGB", (width, height), white)
-    draw = ImageDraw.Draw(image1)
+cv = Canvas(root, width=width*zoom, height=height*zoom, bg=background_color)
 
-    cv.pack(expand=YES, fill=BOTH)
-    cv.bind("<B1-Motion>", paint)
+drawing = PIL.Image.new('RGB', (width, height), background_color)
+draw = ImageDraw.Draw(drawing)
 
-    button = Button(text="submit", command=submit)
-    button.pack()
-    root.mainloop()
+cv.pack(expand=YES, fill=BOTH)
+cv.bind('<B1-Motion>', paint)
+cv.bind('<Button-2>', erase)
+
+button = Button(text='submit', command=submit)
+button.pack()
+root.mainloop()
